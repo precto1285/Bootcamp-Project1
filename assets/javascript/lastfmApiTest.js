c21a1c8c683c44d63503f944af711e4b94fc688f
Jan Balangue <jantonio.balangue@gmail.com>
2018-03-17 12:01:31 -0700

    Modified style.css and lastfmApiTest.js

--- assets/javascript/lastfmApiTest.js
+++ assets/javascript/lastfmApiTest.js
@@ -5,7 +5,9 @@ $(document).ready(function (event) {
     $("#lastFmClick").on("click", function (event) {
         event.preventDefault();
         var artist = $("#lastFmUserInput").val().trim();
+        artist = artist.replace(" ", "+");
         var lastFmURL = "http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=" + artist + "&api_key=5a1f49eb4d70af0cf51958ba989b413c&format=json";
+        artist = artist.replace("+", " ");
         $.ajax({
             type:"GET",
             url: lastFmURL,
@@ -13,15 +15,14 @@ $(document).ready(function (event) {
             dataType: "json",
             success: function(json) {
                         console.log(json);
-                        var newJsonLink = $('<a href="'+ json.results.artistmatches.artist[0].url + '">' + artist + '</a>');
-                        $("#lastFmAPI").append(newJsonLink);
+                        window.location.replace(json.results.artistmatches.artist[0].url + "/+similar");
                         // Parse the response...
                         // Do other things.
                      },
             error: function(xhr, status, err) {
                         // This time, we do not end up here!
                         console.log(err);
-                     }
+            r         }
           });
     });
 });
