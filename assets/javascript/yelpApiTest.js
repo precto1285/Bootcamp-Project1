43d497a6702005a206fc7f45509607eb5fda1b08
Philippe Recto <precto1285@gmal.com>
2018-03-17 10:22:55 -0700

    ticketmasterpull

--- assets/javascript/yelpApiTest.js
+++ assets/javascript/yelpApiTest.js
@@ -6,23 +6,27 @@
 
 //Yelp needs Oauth. May not be good to use...
 
-var url= "https://api.yelp.com/v3/autocomplete?text=del&latitude=37.786882&longitude=-122.399972"
-var apiKey = "gRvBKzmV6dSUtWhQeCEYwaglCqLarBUAz8UwDx2cI0-9LESTPeOGpHoQ4TumROnNJUSMaWlrJ2Uo5XWLmn2GS8O1tzku9tmHdpbiswFVfPmpGL6TkIMW7SMQ5PKrWnYx";
+var tMurl= "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=U7yFX9RveYwkkgtRgbC5I2inlfCqb32G"
+var apiKey = "U7yFX9RveYwkkgtRgbC5I2inlfCqb32G";
 var clientId = "oaIQiNx4zJrgK8w3E8SoCg";
 
 
-    $("#yelpclick").click(function(event){
+    $("#tMasterclick").click(function(event){
         event.preventDefault();
         var input = $("#userInput").val().trim();
       
         $.ajax({
-            //Put the API Key in the request header as "Authorization: Bearer <YOUR API KEY>".
-            url: url,
-            method: 'GET',
-            Authorization: "Bearer" + apiKey,
-            }).done(function (result) {
-                console.log(result);
-                $(".info").append();
-            
-    });
+            type:"GET",
+            url: tMurl,
+            async:true,
+            dataType: "json",
+            success: function(json) {
+                        console.log(json);
+                        // Parse the response.
+                        // Do other things.
+                     },
+            error: function(xhr, status, err) {
+                        // This time, we do not end up here!
+                     }
+          });
 });
