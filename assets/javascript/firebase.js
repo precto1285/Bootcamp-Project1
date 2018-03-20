c00ecf3a24ceba0ed1397b383f2dc21b4570e846
Anthony Edelstein <asedelstein@gmail.com>
2018-03-19 18:27:44 -0700

    git push firebase

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -9,4 +9,56 @@
   };
   firebase.initializeApp(config);
 
-  
+       // define variables
+       var search= "";
+       
+    
+       var database = firebase.database();
+ 
+         // Capture Button Click
+         $("#search").on("click", function (event) {
+         event.preventDefault();
+     
+ 
+         // Grabbed values from text boxes
+         var search = $("#search").val().trim();
+      
+   
+         // Code for handling the push
+         database.ref().push({
+           search: search,
+           dateAdded: firebase.database.ServerValue.TIMESTAMP
+         });
+      
+   
+       // Firebase watcher + initial loader + order/limit HINT: .on("child_added"
+       database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
+     
+   
+         // Console.loging the last user's data
+         console.log(search);
+         
+ 
+       });
+ 
+       dataRef.ref().on("child_added", function(childSnapshot) {
+ 
+              // Log everything that's coming out of snapshot
+              console.log(childSnapshot.val().search);
+             
+       });   
+ 
+       dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
+ 
+         // Change the HTML to reflect
+         $("#search").text(snapshot.val().search);
+
+       });
+   
+     
+ 
+         // Handle the errors
+         }, function(errorObject) {
+         console.log("Errors handled: " + errorObject.code);
+       });
+    
