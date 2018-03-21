754d7c9cbb7bab3511c844e2c2194f36d4eede39
Philippe Recto <precto1285@gmal.com>
2018-03-20 20:07:02 -0700

    tm js refresh

--- assets/javascript/ticketMasterApiTest.js
+++ assets/javascript/ticketMasterApiTest.js
@@ -10,67 +10,66 @@
 var apiKey = "U7yFX9RveYwkkgtRgbC5I2inlfCqb32G";
 console.log("hello");
 
-    $("#tMasterclick").on("click", function(event){
-        event.preventDefault();
-        var input = $("#userInput").val().trim();
-        $.ajax({
-            type:"GET",
-            url:"https://app.ticketmaster.com/discovery/v2/attractions.json?keyword=" + input + "&apikey=" + apiKey,
-            async:true,
-            dataType: "json",
-            success: function(json) {
-                        console.log(json);
-                        
-                        //external link to homepage:
-                        if(json._embedded.attractions[0].hasOwnProperty('externalLinks') && json._embedded.attractions[0].externalLinks.hasOwnProperty('homepage')){
-                                console.log("works");
-                                var homePage = json._embedded.attractions[0].externalLinks.homepage[0].url;
-                                $("#website").html('<a href="'+homePage+'" target="_blank">' + homePage + '</a>');
-                        }           
+$("#tMasterclick").on("click", function (event) {
+    event.preventDefault();
+    var input = $("#userInput").val().trim();
+    $.ajax({
+        type: "GET",
+        url: "https://app.ticketmaster.com/discovery/v2/attractions.json?keyword=" + input + "&apikey=" + apiKey,
+        async: true,
+        dataType: "json",
+        success: function (json) {
+            console.log(json);
 
-                        //external link to instagram:
-                        if(json._embedded.attractions[0].hasOwnProperty('externalLinks') && json._embedded.attractions[0].externalLinks.hasOwnProperty('instagram')){
-                            console.log("works");
-                            var instagram = json._embedded.attractions[0].externalLinks.instagram[0].url;
-                            $("#instagram").html('<a href="'+instagram+'" target="_blank">' +instagram + '</a>');
-                    }   
-                    
-                    //external link to facebook:
-                    if(json._embedded.attractions[0].hasOwnProperty('externalLinks') && json._embedded.attractions[0].externalLinks.hasOwnProperty('facebook')){
-                        console.log("works");
-                        var facebook = json._embedded.attractions[0].externalLinks.facebook[0].url;
-                        $("#facebook").html('<a href="'+facebook+'" target="_blank">' +facebook + '</a>');
-                }    
-                        //$("#instagram").text();
-                        //$("#facebook").text();                    
-                    },
-            error: function(xhr, status, err) {
-                        // This time, we do not end up here!
-                     }
-          });
+            //external link to homepage:
+            if (json._embedded.attractions[0].hasOwnProperty('externalLinks') && json._embedded.attractions[0].externalLinks.hasOwnProperty('homepage')) {
+                console.log("works");
+                var homePage = json._embedded.attractions[0].externalLinks.homepage[0].url;
+                $("#website").html('<a href="' + homePage + '" target="_blank">' + homePage + '</a>');
+            }
 
+            //external link to instagram:
+            if (json._embedded.attractions[0].hasOwnProperty('externalLinks') && json._embedded.attractions[0].externalLinks.hasOwnProperty('instagram')) {
+                console.log("works");
+                var instagram = json._embedded.attractions[0].externalLinks.instagram[0].url;
+                $("#instagram").html('<a href="' + instagram + '" target="_blank">' + instagram + '</a>');
+            }
+
+            //external link to facebook:
+            if (json._embedded.attractions[0].hasOwnProperty('externalLinks') && json._embedded.attractions[0].externalLinks.hasOwnProperty('facebook')) {
+                console.log("works");
+                var facebook = json._embedded.attractions[0].externalLinks.facebook[0].url;
+                $("#facebook").html('<a href="' + facebook + '" target="_blank">' + facebook + '</a>');
+            }
+s
+        },
+        error: function (xhr, status, err) {
+            // This time, we do not end up here!
+        }
+    });
 
 
 
 
 
-        // var tMurl= "https://app.ticketmaster.com/discovery/v2/venues.json?keyword=" + input + "&apikey=";
-        // console.log(input);
-             
-        // $.ajax({
-        //     type:"GET",
-        //     url: tMurl + apiKey,
-        //     async:true,
-        //     dataType: "json",
-        //     success: function(json) {
-        //                 console.log(json);
-                       
-        //              },
-        //     error: function(xhr, status, err) {
-        //                 // This time, we do not end up here!
-        //              }
-        //   });
-        
+
+    // var tMurl= "https://app.ticketmaster.com/discovery/v2/venues.json?keyword=" + input + "&apikey=";
+    // console.log(input);
+
+    // $.ajax({
+    //     type:"GET",
+    //     url: tMurl + apiKey,
+    //     async:true,
+    //     dataType: "json",
+    //     success: function(json) {
+    //                 console.log(json);
+
+    //              },
+    //     error: function(xhr, status, err) {
+    //                 // This time, we do not end up here!
+    //              }
+    //   });
+
 });
 
 
