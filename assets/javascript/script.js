1cc2a6269e462abe67c4bde667da5c5ab1ff97ca
Jan Balangue <jantonio.balangue@gmail.com>
2018-03-27 12:22:26 -0700

    Modified style.css for similar artist text formatting. Modified script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,15 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyACI1VUl9WeNYT0wG8w0SSGW3bjlG-KW2g",
+    authDomain: "bootcamp-project-1-f4a8d.firebaseapp.com",
+    databaseURL: "https://bootcamp-project-1-f4a8d.firebaseio.com",
+    projectId: "bootcamp-project-1-f4a8d",
+    storageBucket: "bootcamp-project-1-f4a8d.appspot.com",
+    messagingSenderId: "765880317002"
+  };
+  firebase.initializeApp(config);
+  
+  
 $("document").ready(function () {
     $(".button-collapse").sideNav();
     $('.slider').slider();
